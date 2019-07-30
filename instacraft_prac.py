c33010a1b30b21ba59b24b3780900ad2f5818c6f
pius338 <ppp37686@gmail.com>
2019-07-31 08:18:44 +0900

    Add game

--- instacraft_prac.py
+++ instacraft_prac.py
@@ -1,3 +1,5 @@
+#from test import *
+from InstagramAPI import InstagramAPI
 import sys
 import FileBrowser
 from PyQt5 import uic
@@ -5,10 +7,41 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import *
 
+import threading
+import datetime
+import time
+import os
+
+
 ProjectUI = "_uiFiles/InstaCraftUI.ui"
 
 
+
 class MainDialog(QDialog):
+    # player1_insta = ['', '']
+    # player2_insta = ['', '']
+    player1_insta_id = ''
+    player1_insta_pw = ''
+    player2_insta_id = ''
+    player2_insta_pw = ''
+
+    # global player1_insta_id
+    # global player1_insta_pw
+    # global player2_insta_id
+    # global player2_insta_pw
+    p1_id = player1_insta_id
+    p1_pw = player1_insta_pw
+
+    p2_id = player2_insta_id
+    p2_pw = player2_insta_pw
+
+
+    p1_api = InstagramAPI(p1_id, p1_pw)
+    p2_api = InstagramAPI(p2_id, p2_pw)
+
+    api = InstagramAPI("sample_id", "sample_pw")
+
+
     def __init__(self):
         QDialog.__init__(self)
         self.ui = uic.loadUi(ProjectUI, self)
@@ -24,35 +57,56 @@ class MainDialog(QDialog):
         #self.Pw_2 = self.ui.player2_pw()
         self.ui.player1_pw.setEchoMode(QLineEdit.Password)
         self.ui.player2_pw.setEchoMode(QLineEdit.Password)
-        self.player1_insta = ['', '']
-        self.player2_insta = ['', '']
+        # global player1_insta
+        # global player2_insta
+        #information of id and pw
+        #self.player1_insta = ['', '']
+        # global player1_insta_id
+        # global player1_insta_pw
+        # global player2_insta_id
+        # global player2_insta_pw
+
+        #self.player2_insta = ['', '']
         self.ui.player1_login_button.pressed.connect(self.GetId_1)
         self.ui.player2_login_button.pressed.connect(self.GetId_2)
-        #self.ui.startbutton.pressed.connect(self.gamemode)
+        self.ui.startbutton.pressed.connect(self.gamemode)
+
         self.ui.show()
 
     #@pyqtSlot()
     def OpenFileBrowser_1(self):
         fileBrowser = FileBrowser.FileDialog()
+        global path_1
         self.path_1 = fileBrowser.openFileNameDialog()
         self.ui.player1_path.setText(self.path_1)
+        p1_pi = path_1
 
     def OpenFileBrowser_2(self):
         fileBrowser = FileBrowser.FileDialog()
+        global path_2
         self.path_2 = fileBrowser.openFileNameDialog()
         self.ui.player2_path.setText(self.path_2)
+        p2_pi = path_2
 
     def GetId_1(self):
-        self.player1_insta[0] = self.ui.player1_id.text()
-        self.player1_insta[1] = self.ui.player1_pw.text()
-        print(self.player1_insta[0])
-        print(self.player1_insta[1])
+        # self.player1_insta[0] = self.ui.player1_id.text()
+        # self.player1_insta[1] = self.ui.player1_pw.text()
+        global player1_insta_id #= self.ui.player1_id.text()
+        player1_insta_id = self.ui.player1_id.text()
+        global player1_insta_pw# = self.ui.player1_pw.text()
+        player1_insta_pw = self.ui.player1_pw.text()
+        # print(self.player1_insta[0])
+        # print(self.player1_insta[1])
 
     def GetId_2(self):
-        self.player2_insta[0] = self.ui.player2_id.text()
-        self.player2_insta[1] = self.ui.player2_pw.text()
-        print(self.player2_insta[0])
-        print(self.player2_insta[1])
+        # self.player2_insta[0] = self.ui.player2_id.text()
+        # self.player2_insta[1] = self.ui.player2_pw.text()
+        global player2_insta_id# = self.ui.player2_id.text()
+        player2_insta_id = self.ui.player2_id.text()
+        global player2_insta_pw# = self.ui.player2_pw.text()
+        player2_insta_pw = self.ui.player2_pw.text()
+        # print(self.player2_insta[0])
+        # print(self.player2_insta[1])
 
     #def Game_Start(self):
         #if
@@ -61,6 +115,118 @@ class MainDialog(QDialog):
     #def function(self):
        # image_path = 'c:\image_path.jpg'  # path to your image file
         #self.show_frame_in_display(image_path)
+    def gamemode(wl):
+        # os.system("start")
+        p3_sc = 0
+        print("ready")
+        for i in range(3):
+            time.sleep(1)
+            print(3 - i)
+        time.sleep(1)
+        print("start")
+
+        sc1 = []
+
+        now = time.time()
+        future = now + 2
+
+        while time.time() < future:
+            innum = input()
+            sc1.append(innum)
+
+        p1_sc = len(sc1)
+        print("stop")
+
+        # print(p1_sc)
+
+        time.sleep(3)
+
+        print("ready")
+        for i in range(3):
+            time.sleep(1)
+
+            print(3 - i)
+        print("start")
+
+        sc2 = []
+
+        now = time.time()
+        future = now + 2
+
+        while time.time() < future:
+            innum = input()
+            sc2.append(innum)
+
+        p2_sc = len(sc2) - p1_sc
+        print("stop")
+        # print(p2_sc)
+        if (p1_sc > p2_sc):
+            # print("player1 win")
+            return p1_sc
+        elif (p1_sc < p2_sc):
+            # print("player2 win")
+            return p2_sc
+        else:
+            # print("draw")
+            return p3_sc
+
+    def check_time(curr_minute):
+        dt = datetime.datetime.now()
+        gaming = gamemode()
+        global p1_sc
+        global p2_sc
+        global p3_sc
+
+        if (gaming == p1_sc):
+            global api
+            global p1_api
+            global p2_api
+
+            api = p2_api
+            # 사진 경로 수정 필요
+            photo_path_1 = 'D:/28750952_126473978106488_9046258563504144384_n.jpg'
+            # 게시글 내용 수정 필요
+            caption = "게임 벌칙으로 사진을 강제 업로드 합니다. - \"Instacraft\""
+            api.uploadPhoto(photo=photo_path_1, caption=caption, upload_id=None)
+            print("Upload succes!")
+
+        elif (gaming == p2_sc):
+            # global api
+            # global p1_api
+            # global p2_api
+
+            api = p1_api
+
+            # 사진 경로 수정 필요
+            photo_path_2 = 'D:/28750952_126473978106488_9046258563504144384_n.jpg'
+            # 게시글 내용 수정 필요
+            caption = "게임 벌칙으로 사진을 강제 업로드 합니다. - \"Instacraft\""
+            api.uploadPhoto(photo=photo_path_2, caption=caption, upload_id=None)
+            print("Upload succes!")
+
+        elif (gaming == p3_sc):
+            # global api
+            # global p1_api
+            # global p2_api
+
+            print("draw, don't upload")
+
+        threading.Timer(1, check_time, args=[curr_minute]).start()
+
+    if (api.login()):
+
+        api.getSelfUserFeed()  # get self user feed
+
+        print(api.LastJson)  # print last response JSON
+
+        print("Login succes!")
+
+    else:
+
+        print("Can't login!")
+        exit()
+
+    check_time(-1)
 
 
 
--- test.py
+++ test.py
@@ -2,6 +2,15 @@ from InstagramAPI import InstagramAPI
 import threading
 import datetime
 import time
+from instacraft_prac import player1_insta_id
+
+from instacraft_prac import player1_insta_pw
+
+from instacraft_prac import player2_insta_id
+
+from instacraft_prac import player2_insta_pw
+
+
 
 #game function
 def gamemode():
@@ -9,7 +18,6 @@ def gamemode():
     print("ready")
     for i in range(3):
         time.sleep(1)
-        
         print(3-i)
     time.sleep(1)
     print("start")
@@ -65,60 +73,66 @@ def gamemode():
     #print(c)
 
 #아이디 비번 사진  입력 구간
-p1_id = "p1_id"
-p1_pw = "p1_pw"
+
+global player1_insta_id
+global player1_insta_pw
+global player2_insta_id
+global player2_insta_pw
+p1_id = player1_insta_id
+p1_pw = player1_insta_pw
 p1_pi = "p1_pi"
 
-p2_id = "p2_id"
-p2_pw = "p2_pw"
+p2_id = player2_insta_id
+p2_pw = player2_insta_pw
 p2_pi = "p2_pi"
 
-p1_api = InstagramAPI("sample_id", "sample_pw")
-p2_api = InstagramAPI("sample_id", "sample_pw")
+p1_api = InstagramAPI(p1_id, p1_pw)
+p2_api = InstagramAPI(p2_id, p2_pw)
 
 api = InstagramAPI("sample_id", "sample_pw")
 
 def check_time(curr_minute):
     dt = datetime.datetime.now()
     gaming = gamemode()
+    global p1_sc
+    global p2_sc
+    global p3_sc
+
     if (gaming == p1_sc):
-        
         global api
         global p1_api
         global p2_api
-        
+
         api = p2_api
         #사진 경로 수정 필요
         photo_path_1 = 'D:/28750952_126473978106488_9046258563504144384_n.jpg'
         #게시글 내용 수정 필요
-        caption = "뻐꾹"*minute + str(minute)
+        caption = "게임 벌칙으로 사진을 강제 업로드 합니다. - \"Instacraft\""
         api.uploadPhoto(photo=photo_path_1, caption=caption, upload_id=None)
         print("Upload succes!")
 
     elif(gaming == p2_sc):
-        global api
-        global p1_api
-        global p2_api
+        # global api
+        # global p1_api
+        # global p2_api
 
         api = p1_api
         
         #사진 경로 수정 필요
         photo_path_2 = 'D:/28750952_126473978106488_9046258563504144384_n.jpg'
         #게시글 내용 수정 필요
-        caption = "게임 벌칙으로 사진을 강제 업로드 합니다. - "Instacraft"
+        caption = "게임 벌칙으로 사진을 강제 업로드 합니다. - \"Instacraft\""
         api.uploadPhoto(photo=photo_path_2, caption=caption, upload_id=None)
         print("Upload succes!")
         
     elif(gaming == p3_sc):
-        global api
-        global p1_api
-        global p2_api
+        # global api
+        # global p1_api
+        # global p2_api
         
         print("draw, don't upload")
 
     threading.Timer(1, check_time, args=[curr_minute]).start()
-
-    
 
 #api = InstagramAPI("sample_id", "sample_pw")
 #로그인 하는 부분 간섭 ㅇㅇ
@@ -138,19 +152,4 @@ else:
 check_time(-1)
 
 
-"""
-#사진 및 게시글 포스팅
 
-InstagramAPI = InstagramAPI("", "")
-
-InstagramAPI.login()  # login
-
-
-#사진 주소 \ 대신 /로
-photo_path ="C:/Users/USER/Desktop/py/photo/black.jpg"
-#게시글
-caption = "Instgram_bot testing_Cecom"
-
-InstagramAPI.uploadPhoto(photo_path, caption=caption)
-
-"""
