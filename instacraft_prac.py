1ade43f0c4efb25ce0619885c3620410ce5a0874
pius338 <ppp37686@gmail.com>
2019-07-31 09:30:41 +0900

    First commit

--- instacraft_prac.py
+++ instacraft_prac.py
@@ -18,6 +18,7 @@ ProjectUI = "_uiFiles/InstaCraftUI.ui"
 
 
 class MainDialog(QDialog):
+    end = 1
     # player1_insta = ['', '']
     # player2_insta = ['', '']
     player1_insta_id = ''
@@ -35,6 +36,11 @@ class MainDialog(QDialog):
     p2_id = player2_insta_id
     p2_pw = player2_insta_pw
 
+    p1_pi = ''
+    p2_pi = ''
+
+    path_1 = ''
+    path_2 = ''
 
     p1_api = InstagramAPI(p1_id, p1_pw)
     p2_api = InstagramAPI(p2_id, p2_pw)
@@ -47,26 +53,13 @@ class MainDialog(QDialog):
         self.ui = uic.loadUi(ProjectUI, self)
 
         self.button_1 = self.ui.player1_picture_search
-        self.path_1 = ''
-        self.path_2 = ''
+
         self.ui.player1_picture_search.pressed.connect(self.OpenFileBrowser_1)
         self.ui.player2_picture_search.pressed.connect(self.OpenFileBrowser_2)
-        #self.Id_1 = self.ui.player1_id()
-        #self.Pw_1 = self.ui.player1_pw()
-        #self.Id_2 = self.ui.player2_id()
-        #self.Pw_2 = self.ui.player2_pw()
+
         self.ui.player1_pw.setEchoMode(QLineEdit.Password)
         self.ui.player2_pw.setEchoMode(QLineEdit.Password)
-        # global player1_insta
-        # global player2_insta
-        #information of id and pw
-        #self.player1_insta = ['', '']
-        # global player1_insta_id
-        # global player1_insta_pw
-        # global player2_insta_id
-        # global player2_insta_pw
 
-        #self.player2_insta = ['', '']
         self.ui.player1_login_button.pressed.connect(self.GetId_1)
         self.ui.player2_login_button.pressed.connect(self.GetId_2)
         self.ui.startbutton.pressed.connect(self.gamemode)
@@ -77,15 +70,19 @@ class MainDialog(QDialog):
     def OpenFileBrowser_1(self):
         fileBrowser = FileBrowser.FileDialog()
         global path_1
-        self.path_1 = fileBrowser.openFileNameDialog()
-        self.ui.player1_path.setText(self.path_1)
-        p1_pi = path_1
+        path_1 = fileBrowser.openFileNameDialog()
+        self.ui.player1_path.setText(path_1)
+        global p1_pi
+        p1_pi= path_1
+
+
 
     def OpenFileBrowser_2(self):
         fileBrowser = FileBrowser.FileDialog()
         global path_2
-        self.path_2 = fileBrowser.openFileNameDialog()
-        self.ui.player2_path.setText(self.path_2)
+        path_2 = fileBrowser.openFileNameDialog()
+        self.ui.player2_path.setText(path_2)
+        global p2_pi
         p2_pi = path_2
 
     def GetId_1(self):
@@ -95,8 +92,15 @@ class MainDialog(QDialog):
         player1_insta_id = self.ui.player1_id.text()
         global player1_insta_pw# = self.ui.player1_pw.text()
         player1_insta_pw = self.ui.player1_pw.text()
-        # print(self.player1_insta[0])
-        # print(self.player1_insta[1])
+        global p1_id
+        global p1_pw
+        p1_id = player1_insta_id
+        p1_pw = player1_insta_pw
+        print(p1_id)
+        print(p1_pw)
+        global p1_api
+        p1_api = InstagramAPI(p1_id, p1_pw)
+
 
     def GetId_2(self):
         # self.player2_insta[0] = self.ui.player2_id.text()
@@ -105,6 +109,12 @@ class MainDialog(QDialog):
         player2_insta_id = self.ui.player2_id.text()
         global player2_insta_pw# = self.ui.player2_pw.text()
         player2_insta_pw = self.ui.player2_pw.text()
+        global p2_id
+        global p2_pw
+        p2_id = player1_insta_id
+        p2_pw = player1_insta_pw
+        global p2_api
+        p2_api = InstagramAPI(p2_id, p2_pw)
         # print(self.player2_insta[0])
         # print(self.player2_insta[1])
 
@@ -160,19 +170,23 @@ class MainDialog(QDialog):
         p2_sc = len(sc2) - p1_sc
         print("stop")
         # print(p2_sc)
+        global end
+        end = 0
         if (p1_sc > p2_sc):
-            # print("player1 win")
+            print("player1 win")
+            print(end)
             return p1_sc
         elif (p1_sc < p2_sc):
-            # print("player2 win")
+            print("player2 win")
+            print(end)
             return p2_sc
         else:
-            # print("draw")
+            print("draw")
             return p3_sc
 
     def check_time(curr_minute):
-        dt = datetime.datetime.now()
-        gaming = gamemode()
+        # dt = datetime.datetime.now()
+        gaming = gamemode(1)
         global p1_sc
         global p2_sc
         global p3_sc
@@ -181,10 +195,12 @@ class MainDialog(QDialog):
             global api
             global p1_api
             global p2_api
+            global p1_pi
+            global p2_pi
 
             api = p2_api
             # 사진 경로 수정 필요
-            photo_path_1 = 'D:/28750952_126473978106488_9046258563504144384_n.jpg'
+            photo_path_1 = p1_pi
             # 게시글 내용 수정 필요
             caption = "게임 벌칙으로 사진을 강제 업로드 합니다. - \"Instacraft\""
             api.uploadPhoto(photo=photo_path_1, caption=caption, upload_id=None)
@@ -198,7 +214,7 @@ class MainDialog(QDialog):
             api = p1_api
 
             # 사진 경로 수정 필요
-            photo_path_2 = 'D:/28750952_126473978106488_9046258563504144384_n.jpg'
+            photo_path_2 = p2_pi
             # 게시글 내용 수정 필요
             caption = "게임 벌칙으로 사진을 강제 업로드 합니다. - \"Instacraft\""
             api.uploadPhoto(photo=photo_path_2, caption=caption, upload_id=None)
@@ -213,20 +229,23 @@ class MainDialog(QDialog):
 
         threading.Timer(1, check_time, args=[curr_minute]).start()
 
-    if (api.login()):
 
-        api.getSelfUserFeed()  # get self user feed
+    if(end == 0):
+        if (api.login()):
 
-        print(api.LastJson)  # print last response JSON
+            api.getSelfUserFeed()  # get self user feed
 
-        print("Login succes!")
+            print(api.LastJson)  # print last response JSON
 
-    else:
+            print("Login succes!")
 
-        print("Can't login!")
-        exit()
+        else:
 
-    check_time(-1)
+            print("Can't login!")
+            exit()
+
+        check_time(-1)
+
 
 
 
